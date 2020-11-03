adcc6638b5969391d72dcab4c1094e65421488d3
sarah-svg <kjskillen@gmail.com>
2020-11-03 07:49:28 -0800

    trying to the the backend up

--- data/create-tables.js
+++ data/create-tables.js
@@ -17,7 +17,7 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE colors (
                     id SERIAL PRIMARY KEY NOT NULL,
                     name VARCHAR(512) NOT NULL,
                     cool_factor INTEGER NOT NULL,
