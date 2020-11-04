044db7010c3560aefcc88cd7b91219986b8e68a7
sarah-svg <kjskillen@gmail.com>
2020-11-03 20:59:42 -0800

    put

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,157 +10,157 @@ describe('app routes', () => {
   describe('routes', () => {
     //let token;
   
-    // beforeAll(async done => {
-    //execSync('npm run setup-db');
+    beforeAll(async done => {
+      execSync('npm run setup-db');
   
-    client.connect();
+      client.connect();
   
-    //const signInData = await fakeRequest(app)
-    ////.post('/auth/signup')
-    //.send({
-    // email: 'jon@user.com',
-    //  password: '1234'
-    /// });
+      //const signInData = await fakeRequest(app)
+      ////.post('/auth/signup')
+      //.send({
+      // email: 'jon@user.com',
+      //  password: '1234'
+      /// });
       
-    //token = signInData.body.token;
+      //token = signInData.body.token;
   
-    return done();
-  });
+      return done();
+    });
   
-  afterAll(done => {
-    return client.end(done);
-  });
+    afterAll(done => {
+      return client.end(done);
+    });
 
-  test('returns colors', async() => {
+    test('returns colors', async() => {
 
-    const expectation = [
-      {
+      const expectation = [
+        {
+          id: 1,
+          name: 'teal',
+          cool_factor: 3,  
+          owner_id: 1,
+          cool: true
+       
+        },
+        {
+          id: 2,  
+          name: 'green',
+          cool_factor: 4, 
+          owner_id: 1,
+          cool: true
+        },
+        {
+          id: 3, 
+          name: 'blue',
+          cool_factor: 10,
+          owner_id: 1,
+          cool: true
+        },
+        {
+          id: 4,
+          name: 'red',
+          cool_factor: 0,
+          cool: false,
+          owner_id: 1
+        }
+      ];
+
+      const data = await fakeRequest(app)
+        .get('/colors')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+  
+
+    test('returns a single color', async() => {
+
+      const expectation = {
         id: 1,
         name: 'teal',
-        cool_factor: 3,  
-        owner_id: 1,
-        cool: true
-       
-      },
-      {
-        id: 2,  
-        name: 'green',
-        cool_factor: 4, 
-        owner_id: 1,
-        cool: true
-      },
-      {
-        id: 3, 
-        name: 'blue',
-        cool_factor: 10,
-        owner_id: 1,
-        cool: true
-      },
-      {
-        id: 4,
-        name: 'red',
-        cool_factor: 0,
-        cool: false,
+        cool_factor: 3,
+        cool: true,
         owner_id: 1
-      }
-    ];
+      };
 
-    const data = await fakeRequest(app)
-      .get('/colors')
-      .expect('Content-Type', /json/)
-      .expect(200);
+      const data = await fakeRequest(app)
+        .get('/colors/1')
+        .expect('Content-Type', /json/)
+        .expect(200);
 
-    expect(data.body).toEqual(expectation);
-  });
-  
-
-  test('returns a single color', async() => {
-
-    const expectation = {
-      id: 1,
-      name: 'teal',
-      cool_factor: 3,
-      cool: true,
-      owner_id: 1
-    };
-
-    const data = await fakeRequest(app)
-      .get('/colors/1')
-      .expect('Content-Type', /json/)
-      .expect(200);
-
-    expect(data.body).toEqual(expectation);
-  });
+      expect(data.body).toEqual(expectation);
+    });
  
 
 
-  test('adds a new color', async() => {
-    const expectation = {
-      id: 5,
-      name: 'orange',
-      cool: false,
-      cool_factor: 2,
-      owner_id: 1
-    };
-
-    const data = await fakeRequest(app)
-      .post('/colors')
-      .send({
-      
+    test('adds a new color', async() => {
+      const expectation = {
+        id: 5,
         name: 'orange',
         cool: false,
         cool_factor: 2,
         owner_id: 1
-      })
-      .expect('Content-Type', /json/)
-      .expect(200);
+      };
 
-    const allColors = await fakeRequest(app)
-      .get('/colors')
+      const data = await fakeRequest(app)
+        .post('/colors')
+        .send({
+      
+          name: 'orange',
+          cool: false,
+          cool_factor: 2,
+          owner_id: 1
+        })
+        .expect('Content-Type', /json/)
+        .expect(200);
 
-      .expect('Content-Type', /json/)
-      .expect(200);
+      const allColors = await fakeRequest(app)
+        .get('/colors')
 
-    expect(data.body).toEqual(expectation);
-    expect(allColors.body.length).toEqual(5);
+        .expect('Content-Type', /json/)
+        .expect(200);
 
-  });
-  test(' updates existing color', async() => {
-    const expectation = {
-      id: 1,
-      name: 'yellow',
-      cool: false,
-      cool_factor: 1,
-      owner_id: 1
+      expect(data.body).toEqual(expectation);
+      expect(allColors.body.length).toEqual(5);
 
-    };
-    const data = await fakeRequest(app)
-      .put('/colors/1')
-      .send({
+    });
+    test(' updates existing color', async() => {
+      const expectation = {
+        id: 1,
         name: 'yellow',
         cool: false,
         cool_factor: 1,
         owner_id: 1
 
-      })
-      .expect('Content-Type', /json/)
-      .expect(200);
+      };
+      const data = await fakeRequest(app)
+        .put('/colors/1')
+        .send({
+          name: 'yellow',
+          cool: false,
+          cool_factor: 1,
+          owner_id: 1
 
-    const updatedColor = await fakeRequest(app)
-      .get('/colors/1')
-      .expect('Content-Type', /json/)
-      .expect(200);
+        })
+        .expect('Content-Type', /json/)
+        .expect(200);
 
-    expect(data.body).toEqual(expectation);
-    expect(updatedColor.body);
+      const updatedColor = await fakeRequest(app)
+        .get('/colors/1')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+      expect(updatedColor.body);
+
+
+    });
+
+
 
 
   });
 
-
-
-
 });
-
-//});
 
